c2c16ffc7e6026a3fde29ffb458cf76428ebee9b
giau.tran <giau.tmg@gmail.com>
2015-11-25 00:28:24 +0700

    ES6 update

--- index.js
+++ index.js
@@ -1,76 +1,105 @@
 /* global Buffer */
+/* global Promise */
 'use strict';
 
-let Promise = require('bluebird');
 let crypto = require('crypto');
 let request = require('request-promise');
 
-let BitcoinVnClient = function (client_key, client_secret, api_url) {
-    this.client_key = client_key;
-    this.client_secret = client_secret;
-    this.api_url = api_url || 'https://www.bitcoinvietnam.com.vn/api/v2';
-}
+class BitcoinVnClient {
 
-BitcoinVnClient.prototype.query = function (options) {
-    
-    options.nonce = Date.now() / 1000 | 0;
+    constructor(client_key, client_secret, api_url) {
+        this.client_key = client_key;
+        this.client_secret = client_secret;
+        this.api_url = api_url || 'https://www.bitcoinvietnam.com.vn/api/v2';
+    }
 
-    let b64Params = new Buffer(JSON.stringify(options)).toString('base64');
-    let signature = crypto.createHmac('sha256', this.client_secret)
-        .update(b64Params)
-        .digest('hex');
-        
-    return request({
-        uri: this.api_url,
-        method: 'POST',
-        headers: {
-            'X-BITCOINVIETNAM-KEY': this.client_key,
-            'X-BITCOINVIETNAM-PARAMS': b64Params,
-            'X-BITCOINVIETNAM-SIGNATURE': signature
-        },
-        form: options
-    })
-    .then(function (jsonString) {
-        /* Loại bỏ khoảng trắng thừa và parse chuỗi JSON. */
-        let rep = JSON.parse(jsonString.replace(/\s+/g,' '));
-        
-        if (rep.error) {
-            return Promise.reject(rep.error);
+    /*
+     * Thực thi một câu truy vấn đến API và trả về kết quả.
+     *
+     * @param: options - object, là các tham số cần thiết để tạo câu truy vấn.
+     *
+     * @code:
+     * query({
+     *    method: 'account'
+     * }).then(console.log);
+     */
+    query(options) {
+        return new Promise(function (resolve, reject) {
+            try {
+                options.nonce = Date.now() / 1000 | 0;
+
+                let b64Params = new Buffer(JSON.stringify(options)).toString('base64');
+                let signature = crypto.createHmac('sha256', this.client_secret)
+                    .update(b64Params)
+                    .digest('hex');
+
+                resolve({
+                    uri: this.api_url,
+                    method: 'POST',
+                    headers: {
+                        'X-BITCOINVIETNAM-KEY': this.client_key,
+                        'X-BITCOINVIETNAM-PARAMS': b64Params,
+                        'X-BITCOINVIETNAM-SIGNATURE': signature
+                    },
+                    form: options
+                });
+            } catch (exception) {
+                reject(exception);
+            }
+        }.bind(this)).then(request).then(function (jsonString) {
+            /* Loại bỏ khoảng trắng thừa và parse chuỗi JSON. */
+            // TODO: Bug - Nếu chuỗi giá trị có nhiều khoảng trắng thì cũng sẽ bị sửa đổi.
+            let rep = JSON.parse(jsonString.replace(/\s+/g, ' '));
+
+            if (rep.error) {
+                return Promise.reject(rep.error);
+            }
+
+            return Promise.resolve(rep.response);
+        });
+    }
+
+    getDailyData() {
+        return this.query({method: 'dailydata'});
+    }
+
+    getAccount() {
+        return this.query({method: 'account'});
+    }
+
+    getStatus() {
+        return this.query({method: 'status'});
+    }
+
+    getTicker() {
+        return this.query({method: 'ticker'});
+    }
+
+    getPrice(side) {
+        if (side !== 'buy' && side !== 'sell') {
+            side = 'buy';
         }
-        
-        return Promise.resolve(rep.response);
-    });
-}
 
-BitcoinVnClient.prototype.getDailyData = function () {
-    return this.query({ method: 'dailydata'});
-};
-BitcoinVnClient.prototype.getAccount = function () {
-    return this.query({ method: 'account'});
-};
-BitcoinVnClient.prototype.getStatus = function () {
-    return this.query({ method: 'status'});
-};
-BitcoinVnClient.prototype.getTicker = function () {
-    return this.query({ method: 'ticker'});
-};
-BitcoinVnClient.prototype.getPrice = function (side) {
-    return this.query({ method: 'price', side: side || 'buy'});
-};
-BitcoinVnClient.prototype.getOrderStatus = function (orderId) {
-    return this.query({ method: 'orderstatus', id: orderId});
-};
-BitcoinVnClient.prototype.sellCoin = function (amount) {
-    return this.query({ method: 'sell', amount: amount});
-};
-BitcoinVnClient.prototype.buyCoin = function (amount, amountType, price, btcAddress) {
-    return this.query({
-         method: 'buy',
-         amount: amount,
-         amountType: amountType,
-         price: price,
-         btcAddress: btcAddress || 'WALLET'
-    });
-};
+        return this.query({method: 'price', side: side});
+    }
+
+    getOrderStatus(orderId) {
+        return this.query({method: 'orderstatus', id: orderId});
+    }
+
+    sellCoin(amount) {
+        return this.query({method: 'sell', amount: amount});
+    }
+
+    buyCoin(amount, amountType, price, btcAddress) {
+        return this.query({
+            method: 'buy',
+            amount: amount,
+            amountType: amountType,
+            price: price,
+            btcAddress: btcAddress || 'WALLET'
+        });
+    }
+}
 
 module.exports = BitcoinVnClient
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var BitcoinVnClient = require('./index.js');
 
 var client = new BitcoinVnClient(
-	Chuoi API KEY,
-	CHUOI SECRECT,
+	'7Vo3PEig0NmuKTdFO6ADkxW2LslG1YHn',
+	'3c56a5f5a0dbe399d982dad3a8dec1893463874102d0677ebf41515454483298afdd98c714fae178c590b536e1143f0c732eea66bab74f62232abeb1ef28992b',
 	null);
 
 
